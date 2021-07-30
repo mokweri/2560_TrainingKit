27628cbc432710450343c9ecca02463918f06173
mokweri <mogsobd@gmail.com>
2021-07-30 17:08:24 +0300

    arduino core

---
+++ Software/MCU Training Kit V1.0/drivers/timer0.c
@@ -0,0 +1,6 @@
+/*
+ * timer0.c
+ *
+ * Created: 30/07/2021 11:54:55
+ *  Author: OBED
+ */ 
---
+++ Software/MCU Training Kit V1.0/drivers/timer0.h
@@ -0,0 +1,16 @@
+/*
+ * timer0.h
+ *
+ * Created: 30/07/2021 11:36:23
+ *  Author: OBED
+ */ 
+
+
+#ifndef TIMER0_H_
+#define TIMER0_H_
+
+
+
+
+
+#endif /* TIMER0_H_ */
