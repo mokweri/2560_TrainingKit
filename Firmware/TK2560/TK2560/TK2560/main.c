d566707b82dc985bc1d95b1445f5626fd2ce083d
mokweri <mogsobd@gmail.com>
2021-08-18 17:10:36 +0300

    update

--- Firmware/TK2560/TK2560/TK2560/main.c
+++ Firmware/TK2560/TK2560/TK2560/main.c
@@ -63,19 +63,19 @@ int main(void)
 	
 
 	//Tone Example	
-// 	for (int thisNote = 0; thisNote < 8; thisNote++) 
-// 	{
-// 		// to calculate the note duration, take one second divided by the note type.
-// 		//e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-// 		int noteDuration = 1000 / noteDurations[thisNote];
-// 		tone(Pin_PE3, melody[thisNote], noteDuration);
-// 		// to distinguish the notes, set a minimum time between them.
-// 		// the note's duration + 30% seems to work well:
-// 		int pauseBetweenNotes = noteDuration * 1.30;
-// 		delay_ms(pauseBetweenNotes);		
-// 		// stop the tone playing:
-// 		noTone(Pin_PE3);
-// 	}
+	for (int thisNote = 0; thisNote < 8; thisNote++) 
+	{
+		// to calculate the note duration, take one second divided by the note type.
+		//e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+		int noteDuration = 1000 / noteDurations[thisNote];
+		tone(Pin_PE3, melody[thisNote], noteDuration);
+		// to distinguish the notes, set a minimum time between them.
+		// the note's duration + 30% seems to work well:
+		int pauseBetweenNotes = noteDuration * 1.30;
+		delay_ms(pauseBetweenNotes);		
+		// stop the tone playing:
+		noTone(Pin_PE3);
+	}
 
     while (1) 
     {
